19cd413afb5dbd4bc197680d877e4096a218cb39
JonasMa <github@jmattes.de>
2024-04-28 17:20:28 +0200

    Index Page: Basic styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
     `./src/components/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        '2-auto': 'repeat(2, auto)',
+      }
+    },
+    colors: {
+      'blue-dark': '#023047',
+      'blue-light': '#8ECAE6',
+      'white': '#FFF',
+      'yellow': '#FFB703',
+      'yellow-light': '#FFB7031A',
+    },
     fontFamily: {
       sans: ['Inter', 'sans-serif'],
       mono: ['Mono', 'serif'],
