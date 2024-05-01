a8d931b1fc39deb90d9b646d797d0361cdc48ea3
JonasMa <github@jmattes.de>
2024-05-01 23:52:50 +0200

    Prevent small scrolling jump of right side and adjust paddings to be more uniform

--- src/components/cv.tsx
+++ src/components/cv.tsx
@@ -26,8 +26,8 @@ const CV: React.FC = () => {
   );
 
   return (
-    <div className="lg:flex gap-8 px-4 lg:pt-12 container mx-auto min-h-screen">
-      <div className="py-20 lg:sticky lg:top-0 lg:self-start lg:w-1/2 lg:min-h-screen flex flex-col gap-4 lg:justify-between">
+    <div className="lg:flex gap-8 px-4 container mx-auto min-h-screen">
+      <div className="py-20 lg:pt-32 lg:sticky lg:top-0 lg:self-start lg:w-1/2 lg:min-h-screen flex flex-col gap-4 lg:justify-between">
         <header>
             <h1 className="text-6xl font-bold text-yellow">Jonas Mattes</h1>
             <h2 className="font-mono text-blue-light">
@@ -39,7 +39,7 @@ const CV: React.FC = () => {
           <Link to="/impressum">Impressum</Link>
         </footer>
       </div>
-      <main className="lg:py-36 lg:w-1/2 font-mono text-sm text-white overflow-y-auto flex-shrink-0">
+      <main className="lg:pt-36 lg:pb-20 lg:w-1/2 font-mono text-sm text-white overflow-y-auto flex-shrink-0">
         <h3 className="lg:hidden font-sans text-yellow mb-5 text-base">
           About
         </h3>
@@ -54,7 +54,7 @@ const CV: React.FC = () => {
         <section>
           <Jobs jobs={jobs} />
         </section>
-        <section className="mt-16 mb-8 font-sans text-blue-light">
+        <section className="mt-16 font-sans text-blue-light">
           This website is 100% self built in <strong>React</strong> with <strong>Gatsby</strong> and <strong>Tailwind CSS</strong>. Check it out on my <strong className="underline"><a href="https://github.com/JonasMa/se_portfolio" target="_blank">GitHub</a></strong> if you're interested.<br/>
           The website design and functionality is heavily inspired by <a href="https://brittanychiang.com/" target="blank" className="underline">Brittany Chiang</a>.
         </section>
