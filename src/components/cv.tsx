cd2a443e3e7ecc30f3a38d395f0adeea1d699e74
JonasMa <github@jmattes.de>
2024-05-06 16:30:00 +0200

    Refactor disclaimer to declutter

--- src/components/cv.tsx
+++ src/components/cv.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Link } from 'gatsby';
 import SocialMediaIcons from './social';
 import Loader from './loader';
+import Disclaimer from './disclaimer';
 
 // Jobs does some JSON parsing and may block inital render.
 const Jobs = React.lazy(() => import('./jobs'));
@@ -46,8 +47,7 @@ const CV: React.FC = () => {
           </React.Suspense>
         </section>
         <section className="mt-16 font-sans text-blue-light">
-          This website is 100% self built in <strong>React</strong> with <strong>Gatsby</strong> and <strong>Tailwind CSS</strong>. Check it out on my <strong className="underline"><a href="https://github.com/JonasMa/se_portfolio" target="_blank">GitHub</a></strong> if you're interested.<br/>
-          The website design and functionality is heavily inspired by <a href="https://brittanychiang.com/" target="blank" className="underline">Brittany Chiang</a>.
+          <Disclaimer />
         </section>
       </main>
     </div>
---
+++ src/components/disclaimer.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+const Disclaimer: React.FC = () => {
+  return (
+    <>
+      This website is 100% self built in <strong>React</strong> with{' '}
+      <strong>Gatsby</strong> and <strong>Tailwind CSS</strong>. Check it out on
+      my{' '}
+      <strong className="underline">
+        <a href="https://github.com/JonasMa/se_portfolio" target="_blank">
+          GitHub
+        </a>
+      </strong>{' '}
+      if you're interested.
+      <br />
+      The website design and functionality is heavily inspired by{' '}
+      <a
+        href="https://brittanychiang.com/"
+        target="blank"
+        className="underline"
+      >
+        Brittany Chiang
+      </a>
+      .
+    </>
+  );
+};
+
+export default Disclaimer;
