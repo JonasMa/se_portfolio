9d8d208fa3bc18a44b84257f8652131c4f76339b
JonasMa <github@jmattes.de>
2024-05-13 16:50:23 +0200

    Add meta description to page metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql, useStaticQuery } from 'gatsby';
 import type { HeadFC, PageProps } from 'gatsby';
 import CV from '../components/cv';
 import SocialMediaIcons from '../components/social';
@@ -7,11 +7,11 @@ import Menu, { MenuItem } from '../components/menu';
 
 const IndexPage: React.FC<PageProps> = () => {
   const [selectedItem, setSelectedItem] = useState<MenuItem>('about');
-  const scrollTo = useRef<MenuItem|undefined>(undefined);
+  const scrollTo = useRef<MenuItem | undefined>(undefined);
   const onItemSelected = (item: MenuItem) => {
     setSelectedItem(item);
     scrollTo.current = item;
-  }
+  };
 
   return (
     <div className="lg:flex gap-8 px-4 container mx-auto min-h-screen">
@@ -25,11 +25,16 @@ const IndexPage: React.FC<PageProps> = () => {
         <Menu selectedItem={selectedItem} onItemSelected={onItemSelected} />
         <footer className="flex gap-12 font-mono text-blue-light ">
           <SocialMediaIcons />
-          <Link to="/impressum" className="hover:text-yellow">Impressum</Link>
+          <Link to="/impressum" className="hover:text-yellow">
+            Impressum
+          </Link>
         </footer>
       </div>
       <main className="lg:pt-36 lg:pb-20 lg:w-3/5 font-mono text-sm text-white overflow-y-auto flex-shrink-0">
-        <CV scrollToSection={scrollTo.current} onScrolledIntoView={setSelectedItem} />
+        <CV
+          scrollToSection={scrollTo.current}
+          onScrolledIntoView={setSelectedItem}
+        />
       </main>
     </div>
   );
@@ -37,9 +42,21 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage;
 
-export const Head: HeadFC = () => (
-  <>
-    <title>Jonas Mattes</title>
-    <html lang="en" />
-  </>
-);
+export const Head: HeadFC = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          description
+        }
+      }
+    }
+  `);
+  return (
+    <>
+      <title>Jonas Mattes</title>
+      <html lang="en" />
+      <meta name="description" content={data.site.siteMetadata.description} />
+    </>
+  );
+};
